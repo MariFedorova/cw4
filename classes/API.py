64e126203cdb0e28bb931579c3098878ac956bb0
Mariia Fedorova <000maria.fed@gmail.com>
2023-08-01 06:14:35 +0300

    add API, vacancy classes

---
+++ classes/API.py
@@ -0,0 +1,63 @@
+import requests
+from abc import ABC, abstractmethod
+import os
+
+URL_HH = 'https://api.hh.ru/vacancies'
+URL_SJ = 'https://api.superjob.ru/2.0/vacancies/'
+SUPERJOB_API = "v3.r.137693077.1be4c5ca3276df7e216e968fdb6766cbafb1cb18.9a6d59398c153f8b23762af4f39ed7fe3fbebee8"
+
+
+class VacancyAPI(ABC):
+    @abstractmethod
+    def get_vacancies(self, name):
+        pass
+
+
+class HHAPI(VacancyAPI):
+    def get_vacancies(self, name):
+        params = {
+            'text': f'NAME:{name}',
+            'page': 0,
+            'per_page': 100,
+            'only_with_salary': True
+        }
+
+        response = requests.get(URL_HH, params)
+        data = response.json()['items']
+        page_count = 1
+        if len(data) == 100:
+            while True:
+                params["page"] = page_count
+                response = requests.get(URL_HH, params)
+                new_data = response.json()['items']
+                data += new_data
+                if len(new_data) == 100:
+                    page_count += 1
+                else:
+                    break
+        return data
+
+class SJAPI(VacancyAPI):
+    def get_vacancies(self, name):
+        headers = {'X-Api-App-Id': SUPERJOB_API}
+        params = {
+            'keywords': name,
+            'page': 0,
+            'count': 100,
+            'no_agreement': 1
+        }
+
+        response = requests.get(URL_SJ, headers=headers, params=params)
+        data = response.json()['objects']
+        page_count = 1
+        if len(data) == 100:
+            while True:
+                params["page"] = page_count
+                response = requests.get(URL_SJ, headers=headers, params=params)
+                new_data = response.json()['objects']
+                data += new_data
+                if len(new_data) == 100:
+                    page_count += 1
+                else:
+                    break
+        return data
